27e6f59efe132d374370d2cfb254f114c33d8a07
feldofeldo <foldes@virginia.edu>
2024-11-23 08:17:01 -0500

    Updates

---
+++ precache-manifest.aa8678159a97ea9a4a0d7e5b769cd329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b0581f28c52be0871af9a7d6213955",
+    "url": "/pokus2/index.html"
+  },
+  {
+    "revision": "d8c797df0464f9c70ed9",
+    "url": "/pokus2/static/css/2.b6d150ae.chunk.css"
+  },
+  {
+    "revision": "4b2c5afc9c06210f6e35",
+    "url": "/pokus2/static/css/main.7b52e7e6.chunk.css"
+  },
+  {
+    "revision": "d8c797df0464f9c70ed9",
+    "url": "/pokus2/static/js/2.cb110eff.chunk.js"
+  },
+  {
+    "revision": "4b2c5afc9c06210f6e35",
+    "url": "/pokus2/static/js/main.c83b1bb0.chunk.js"
+  },
+  {
+    "revision": "bc15f2c42b122cc05592",
+    "url": "/pokus2/static/js/runtime-main.513f7931.js"
+  }
+]);
--- src/constants.tsx
+++
@@ -1,6 +0,0 @@
-// GAME
-export const NUM_ROUNDS = 20;
-export const TOTAL_ROUNDS = 100;
-
-// FIELDS
-export const LOCAL_STORE_STATE = 'reduxState';
